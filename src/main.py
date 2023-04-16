10f9482518b401e8219d4196b54272de8a9decda
Daniel Skrobot <dd7412506@gmail.com>
2023-04-16 22:36:09 +0200

    added not working feature

--- src/main.py
+++ src/main.py
@@ -7,8 +7,44 @@ import matplotlib.pyplot as plt
 from PIL import Image
 
 import numpy as np
+import cv2
+import tensorflow as tf
+
+IMAGE_WIDTH: int = 224
+IMAGE_HEIGHT: int = 224
 
 CATEGORIES: Dict[int, str] = { 0: 'biological', 1: 'glass', 2: 'metal', 3: 'paper', 4: 'plastic'}
+
+images = []
+
+
+
+def preprocess_image():
+    pass
+
+def load_and_merge_dataset():
+    
+    current_dir: str = os.getcwd()
+    data_dir: str =os.path.join(os.path.dirname(current_dir), 'data')
+    
+    if os.path.isdir(data_dir):
+        list_class_dirs_names: List[str] = os.listdir(data_dir)
+        for current_dir_name in list_class_dirs_names:
+            
+            current_dir: str = os.path.join(data_dir, current_dir_name)
+            
+            list_of_files: List[str] = os.listdir(current_dir)
+        
+            for current_file_name in list_of_files:
+                
+                path_to_file: str = os.path.join(current_dir, current_file_name)
+                
+                img = Image.open(path_to_file)
+                img = img.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
+                img_array = np.array(img)
+                # Normalized the pixel values between 0 and 1 by dividing them by 255.0
+                img_array = img_array / 255.0
+                images.append(img_array)
 
 
 def check_image_sizes() -> List[Tuple[int, int]]:
@@ -81,6 +117,16 @@ if __name__ == '__main__':
 
     min_image_size = find_min_image_size(image_sizes)
     print(min_image_size)
+
+
+    load_and_merge_dataset()
+    print(images[0].shape)
+    images_array = np.array(images)
+
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((train_examples, train_labels))
+    test_dataset = tf.data.Dataset.from_tensor_slices((test_examples, test_labels))
+
 
 
 
