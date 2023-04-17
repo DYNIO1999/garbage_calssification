56421a924114050481950aee5272bc0e3136a525
Daniel Skrobot <dd7412506@gmail.com>
2023-04-17 11:55:23 +0200

    first loading data idea

--- src/main.py
+++ src/main.py
@@ -3,9 +3,6 @@ from typing import List, Tuple, Set, Dict
 import os, os.path
 
 import matplotlib.pyplot as plt
-
-from PIL import Image
-
 import numpy as np
 import cv2
 import tensorflow as tf
@@ -15,41 +12,20 @@ IMAGE_HEIGHT: int = 224
 
 CATEGORIES: Dict[int, str] = { 0: 'biological', 1: 'glass', 2: 'metal', 3: 'paper', 4: 'plastic'}
 
-images = []
-
-
-
 def preprocess_image():
     pass
 
-def load_and_merge_dataset():
-    
+def load_dataset():
     current_dir: str = os.getcwd()
     data_dir: str =os.path.join(os.path.dirname(current_dir), 'data')
-    
-    if os.path.isdir(data_dir):
-        list_class_dirs_names: List[str] = os.listdir(data_dir)
-        for current_dir_name in list_class_dirs_names:
-            
-            current_dir: str = os.path.join(data_dir, current_dir_name)
-            
-            list_of_files: List[str] = os.listdir(current_dir)
-        
-            for current_file_name in list_of_files:
-                
-                path_to_file: str = os.path.join(current_dir, current_file_name)
-                
-                img = Image.open(path_to_file)
-                img = img.resize((IMAGE_WIDTH, IMAGE_HEIGHT))
-                img_array = np.array(img)
-                # Normalized the pixel values between 0 and 1 by dividing them by 255.0
-                img_array = img_array / 255.0
-                images.append(img_array)
+    data = tf.keras.utils.image_dataset_from_directory(data_dir)
+    data_iterator = data.as_numpy_iterator()
 
+    
 
 def check_image_sizes() -> List[Tuple[int, int]]:
     
-    sizes: Set[Tuple(int,int)] = set()
+    sizes: Set[Tuple[int, int]] = set()
         
     current_dir: str = os.getcwd()
     data_dir: str =os.path.join(os.path.dirname(current_dir), 'data')
@@ -66,10 +42,8 @@ def check_image_sizes() -> List[Tuple[int, int]]:
                 
                 path_to_file: str = os.path.join(current_dir, current_file_name)
                 
-                image = Image.open(path_to_file)
-
-                width, height = image.size
-
+                image = cv2.imread(path_to_file)
+                height, width, channels = image.shape
                 sizes.add((width, height))
     
     image_sizes_list = list(sizes) 
@@ -98,7 +72,7 @@ def find_min_image_size(image_sizes: List[Tuple[int,int]]) -> Tuple[int,int]:
 
 if __name__ == '__main__':
     
-    image_sizes: List[Tuple[int,int]] =  check_image_sizes()
+    image_sizes: List[Tuple[int,int]] = check_image_sizes()
     
 
     image_size_count: int = len(image_sizes)
@@ -107,7 +81,7 @@ if __name__ == '__main__':
     
     for size in image_sizes:
         
-        width,height = size 
+        width, height = size
 
         #print(f"{width}, {height}")
 
@@ -118,11 +92,7 @@ if __name__ == '__main__':
     min_image_size = find_min_image_size(image_sizes)
     print(min_image_size)
 
-# Doesnt work
-    #load_and_merge_dataset()
-    #print(images[0].shape)
-    #images_array = np.array(images)
-
+    load_dataset()
 
 
 
