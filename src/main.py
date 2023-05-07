6447fa2642d47437668c85a5b663e6525dcccb72
Daniel Skrobot <dd7412506@gmail.com>
2023-05-07 12:53:52 +0200

    test

--- src/main.py
+++ src/main.py
@@ -317,13 +317,13 @@ def get_orginal_dataset_size() -> int:
 def create_cnn_model(visualize = False):
     model = Sequential()
 
-    model.add(Conv2D(32, kernel_size=(3, 3), padding='same', input_shape=(256, 256, 3), activation='relu'))
+    model.add(Conv2D(32, kernel_size=(7, 7), padding='same', input_shape=(256, 256, 3), activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
     model.add(Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu'))
     model.add(MaxPooling2D(pool_size=2))
 
     model.add(Flatten())
-    model.add(Dense(64, activation='relu'))
+    model.add(Dense(128, activation='relu'))
     model.add(Dropout(0.2))
     model.add(Dense(64, activation='relu'))
     model.add(Dropout(0.2))
